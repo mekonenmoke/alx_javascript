055f86aa15adbf7214bb69f8382cee5d6372a63f
mekonenmoke <mekoninmoke@gmail.com>
2023-09-03 04:35:15 -0900

    JavaScript basics

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+console.log("C is fun");
+console.log("ython is cool");
+console.log("JavaScript is amazing");
---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+let myVariable = ["C is fun", "Python is cool", "JavaScript is amazing"];
+for (var i = 0; i < myVariable.length; i++) {
+    console.log(myVariable[i]);
+}
