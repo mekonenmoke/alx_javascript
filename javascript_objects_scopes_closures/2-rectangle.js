8f2d1fbc342fdd6ed7dded5a181ee127de7bb6f0
mekonenmoke <mekoninmoke@gmail.com>
2023-09-07 10:01:03 -0900

    conditional in constructor

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/js
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      // Create an empty object if width or height is invalid
+      return "Rectangle {}";
+    }
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
