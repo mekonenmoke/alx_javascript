6829c7873517438e2c48dd8262046c546dc457cc
mekonenmoke <mekoninmoke@gmail.com>
2023-09-07 10:09:18 -0900

    function in class

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+class Rectangle {
+    constructor(w, h) {
+        if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+            // Create an empty object if width or height is invalid
+            return "Rectangle {}";
+        }
+        this.width = w;
+        this.height = h;
+    }
+    print() {
+        for (let i = 0; i < this.height; i++) {
+          console.log('X'.repeat(this.width));
+        }
+      }    
+}
+module.exports = Rectangle;
