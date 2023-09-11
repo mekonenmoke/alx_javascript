852a2af961dcff66c9015d966397629666c073a5
mekonenmoke <mekoninmoke@gmail.com>
2023-09-11 00:03:09 -0900

    script that prints all characters of a Star Wars movie

---
+++ javascript-network/5-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(apiUrl, { encoding: 'UTF-8' }, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  const movie = JSON.parse(body);
+  const characters = movie.characters;
+
+  characters.forEach((characterUrl) => {
+    request.get(characterUrl, { encoding: 'UTF-8' }, (error, response, body) => {
+      if (error) {
+        console.error('Error:', error);
+        return;
+      }
+
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  });
+});
