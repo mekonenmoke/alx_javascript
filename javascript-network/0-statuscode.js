325d8504b8db38027a7d511d844d737cdb4600b4
mekonenmoke <mekoninmoke@gmail.com>
2023-09-10 23:01:38 -0900

    Status Code

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const req = require('request');
-const args = process.argv[2];
-req.get(args, {encoding: 'UTF-8'})
+
+req.get(process.argv[2], {encoding: 'UTF-8'})
 .on('response' , (response) => {
     console.log("code: ",response.statusCode);
 })
