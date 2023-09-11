00f522378aaefb74499d82611305ceaf7966c70a
mekonenmoke <mekoninmoke@gmail.com>
2023-09-10 23:56:57 -0900

    Completed Tasks

---
+++ javascript-network/4-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request.get(apiUrl, { encoding: 'UTF-8' }, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  const todos = JSON.parse(body);
+  const completedTasksByUser = {};
+
+  todos.forEach((todo) => {
+    if (todo.completed) {
+      if (completedTasksByUser[todo.userId]) {
+        completedTasksByUser[todo.userId]++;
+      } else {
+        completedTasksByUser[todo.userId] = 1;
+      }
+    }
+  });
+
+  const jsonResult = JSON.stringify(completedTasksByUser, null, 2);
+  console.log(jsonResult);
+});
