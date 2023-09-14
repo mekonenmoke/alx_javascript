af314774912a13b0cf0d2e9401d5e38d9bbd8d67
mekonenmoke <mekoninmoke@gmail.com>
2023-09-14 10:10:38 -0900

    function in js

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const welcomeMessage = (fullName) => {
+    alert(`Welcome ${fullName}`);
+}
+const guillaume = welcomeMessage('Guillaume');
+const alex = welcomeMessage('Alex');
+const fred = welcomeMessage('Fred');
+guillaume();
+alex();
+fred();
