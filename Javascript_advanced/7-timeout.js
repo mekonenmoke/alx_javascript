5ead8372fcd1413fa9f7d7de6f8d49a91bb8bfb8
mekonenmoke <mekoninmoke@gmail.com>
2023-09-14 09:46:07 -0900

    function order

---
+++ Javascript_advanced/7-timeout.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+console.log('Start of the execution queue');
+
+setTimeout(() => {
+  console.log('Final code block to be executed using setTimeout (with delay of 0)');
+}, 0);
+
+for (let i = 0; i < 100; i++) {
+  console.log(i);
+}
+
+console.log('End of the loop printing');
