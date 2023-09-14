b6623f8eddd3f607d007991761e04a5dd90bf97a
mekonenmoke <mekoninmoke@gmail.com>
2023-09-14 08:26:43 -0900

    Maths challenge in js function

---
+++ Javascript_advanced/4-math.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const divideBy = (firstNumber) => {
+    return (secondNumber) => {
+        return secondNumber / firstNumber;
+    }
+}
+const addBy = (firstNumber) => {
+    return (secondNumber) => {
+        return firstNumber + secondNumber;
+    }
+}
+const addBy100 = addBy(100);
+const addBy1000 = addBy(1000);
+const divideBy10 =divideBy(10);
+const divideBy100 = divideBy(100);
+
+console.log(addBy100(20));
+console.log(divideBy10(20));
+console.log(divideBy100(200));
+console.log(addBy1000(20));
